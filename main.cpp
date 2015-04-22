dcda01a745accdb777b700d1494517dd70df90bd
Nathan <nathantaylor50@msn.com>
2015-04-22 11:09:28 +0100

    fixed glfw linking and changed makefile

---
+++ main.cpp
@@ -0,0 +1,24 @@
+/*
+ * main.cpp
+ *
+ *  Created on: 22 Apr 2015
+ *      Author: nathan
+ */
+
+// GLEW
+#define GLEW_STATIC
+#include <GL/glew.h>
+// GLFW
+#include <GLFW/glfw3.h>
+
+int main()
+{
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
+
+    return 0;
+}
--- shaders/fragment.fs
+++
@@ -1,9 +0,0 @@
-#version 130
-
-in vec3 frag_color;
-out vec4 out_color;
-
-void main()
-{
-        out_color = vec4(frag_color, 1.0f);
-}
